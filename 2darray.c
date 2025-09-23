af5c4facb2c60e9ee81d63a72d93a1db001b78fc
siyarampatel <siyarampateljbp@gmail.com>
2025-09-23 15:59:09 +0530

    codes

--- 2darray.c
+++ 2darray.c
@@ -1,4 +1,4 @@
-// #include<stdio.h>
+ #include<stdio.h>
 // int main(){
 //     int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
 //     for(int i=0; i<3; i++){
@@ -49,3 +49,29 @@
 //     return 0;
 // }
 
+// int main(){
+//     int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+//     int brr[20];
+//     int k=0;
+//     for(int i=0; i<3; i++){
+//         for(int j=0; j<3; j++){
+//             brr[k]=arr[i][j];
+//             k++;
+//         }
+//     }
+//     for(int l=0; l<9; l++){
+//         printf("%d ",brr[l]); 
+//     }
+// }
+
+
+// int main(){
+//     int arr[3][3]={{1,2,3},{4,5,6},{7,8,9}};
+//     for(int i=0; i<3; i++){
+//         for(int j=0; j<3; j++){
+//             if(i==j)
+//             printf("%d ",arr[i][j]);
+//         }
+//         printf("\n");
+//     }
+// }
