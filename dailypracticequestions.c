c728c9f9334bf75e78e648c8b73ee92d08d9e42c
siyarampatel <siyarampateljbp@gmail.com>
2025-10-10 12:30:29 +0530

    code

--- dailypracticequestions.c
+++ dailypracticequestions.c
@@ -1,30 +1,57 @@
-//Q1=>Find the maximum and minimum element in an array        
+// //Q1=>Find the maximum and minimum element in an array        
+
+// #include<stdio.h>
+// int main(){
+//     printf("Enter size of array: ");
+//      int n;
+//     scanf("%d",&n);
+//     int a[n];
+//     printf("Enter elements in array:");
+//     for(int i=0; i<n; i++){
+//         scanf("%d",&a[i]);
+//     }
+//     int max = a[0];
+//     for(int i=0; i<n; i++) {
+//         if(a[i]>max){
+//             max = a[i];
+//         }
+        
+//     }
+//     printf("The maximum element is %d\n",max);
+//     int min = a[0];
+//     for(int i=0; i<n; i++) {
+//         if(a[i]<min){
+//             min = a[i];
+//         }
+        
+//     }
+//     printf("The minimum element is %d",min);
+
+// }
+
+//Q2=> Reverse an array in place
 
 #include<stdio.h>
 int main(){
+    int n;
     printf("Enter size of array: ");
-     int n;
     scanf("%d",&n);
-    int a[n];
-    printf("Enter elements in array:");
+    int arr[n];
+    printf("Enter elements in array:\n");
     for(int i=0; i<n; i++){
-        scanf("%d",&a[i]);
+        scanf("%d",&arr[i]);
     }
-    int max = a[0];
-    for(int i=0; i<n; i++) {
-        if(a[i]>max){
-            max = a[i];
-        }
-        
-    }
-    printf("The maximum element is %d\n",max);
-    int min = a[0];
-    for(int i=0; i<n; i++) {
-        if(a[i]<min){
-            min = a[i];
-        }
-        
-    }
-    printf("The minimum element is %d",min);
+    //for revresing of array
 
+    int temp;
+    for(int i=0; i<n/2; i++){
+        temp = arr[i];
+        arr[i] = arr[n-i-1];
+        arr[n-i-1] = temp;
+        
+    }
+    //Printing of array 
+    for(int i=0; i<n; i++){
+        printf("%d ",arr[i]);
+    }
 }
