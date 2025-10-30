880823eefc7e18428ba34b888666f2ea01f3f380
siyarampatel <siyarampateljbp@gmail.com>
2025-10-30 16:06:23 +0530

    code

---
+++ linkedlist3.cpp
@@ -0,0 +1,37 @@
+#include<iostream>
+using namespace std;
+struct scll{
+    int data;
+    scll *next;
+};
+scll *first,*temp,*ttemp;
+void create_first(){
+    first = new scll;
+    cin>>first->data;
+    first->next=first;
+
+    void add_node(){
+        temp=first;
+        while(temp->next!=first){
+            temp=temp->next;
+        }
+            ttemp=new scll;
+            cin>>ttemp->data;
+            ttemp->next=first;
+            temp->next=ttemp;
+        
+    }
+    void addnode_beforefirst(){
+        temp=first;
+        while(temp->next!=first){
+            
+        }
+    }
+    void display(){
+        temp=first;
+        do{
+            cout<<temp->data;
+            temp=temp->next;
+        }while(temp!=first);
+    }
+}
---
+++ recursion.c
@@ -0,0 +1,81 @@
+// #include<stdio.h>
+// void sum(int a){
+//     if(a==0){
+//         return;
+//     }
+//     printf("%d",a);
+//     sum(a-1);
+    
+// }
+// int main(){
+//     sum(3);
+// }
+
+//sum of n numbers 
+
+
+// #include<stdio.h>
+// int sum(int a){
+//     if(a==0){
+//     return 0;
+//     }
+// return a+sum(a-1);
+// }
+// int main(){
+//     int b = sum(5);
+//     printf("The sum is: %d",b);
+// }
+
+
+                    //Factorial 
+
+
+
+// #include<stdio.h>
+// int factorial(int n){
+//     if(n==1){
+//         return 1;
+//     }
+//     return n*factorial(n-1);
+// }
+// int main(){
+//     int n;
+//     printf("Enter n: ");
+//     scanf("%d",&n);
+//     int a = factorial(n);
+//     printf("The factorial is: %d",a);
+// }
+
+//Calculate the power of two(2)
+
+// #include<stdio.h>
+// int power(int n){
+//     if(n==1){
+//         return 2;
+//     }
+//     return 2*power(n-1);
+// }
+// int main(){
+//     int n;
+//     printf("Enter n for raise to the power 2: ");
+//     scanf("%d",&n);
+//     int a = power(n);
+//     printf("The power of 2 to the power n: %d",a);
+// }
+
+//Sum 0f array through recursion
+
+#include<stdio.h>
+int sum(int arr[],int n){
+    if(n==0){
+        return 0;
+    }
+    return arr[n-1]+sum(arr,n-1);
+}
+
+int main(){
+    int arr[]={1,2,3,4,5};
+    int n=5;
+    int a=sum( arr,n);
+    printf("%d",a);
+}
